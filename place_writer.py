362a0b61bddcb765b5fa7062ca0361b1a67e1009
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2018-03-01 22:00:48 +0800

    place writer now can drive chesses anywhere and equal change to both players.still can not write files

--- place_writer.py
+++ place_writer.py
@@ -1,5 +1,47 @@
 from FiveElements import *
+from math import copysign
 
+def move (tryblock,firstpos,swap=False,speed=30):
+    '''To move a chess to another place and make the animation.
+
+This function needs 3 arguments: tryblock,firstpos,boomit(a boolean)'''
+
+    
+    tempObj=posdic[firstpos].chess
+    if swap:
+        tempObj2=posdic[tryblock].chess
+        if posdic[firstpos].type!=posdic[tryblock].type:
+            tempObj2.actpic=tempObj2.six_pic if posdic[firstpos].type=='Six'\
+                        else tempObj2.eight_pic
+    if posdic[firstpos].type!=posdic[tryblock].type:
+        tempObj.actpic=tempObj.six_pic if posdic[tryblock].type=='Six'\
+                        else tempObj.eight_pic
+    if posdic[tryblock].chess!= None:
+        posdic[tryblock].chess.alive=False
+        posdic[tryblock].chess= None
+    posdic[firstpos].chess=None
+    def animate(firstpos,tryblock,tempObj):
+        flag=True
+        oldx,oldy=posdic[firstpos].coords
+        newx,newy=posdic[tryblock].coords
+        movevecter=Vecter2((newx-oldx),(newy-oldy))
+        movevecter.normalize()
+        while flag:
+            oldx+=copysign(min(abs(speed*movevecter.x),abs(newx-oldx)),movevecter.x)
+            oldy+=copysign(min(abs(speed*movevecter.y),abs(newy-oldy)),movevecter.y)
+            DISPLAYSURF.blit(background,(0,0))
+            DISPLAYSURF.blit(tempObj.actpic,(oldx,oldy))
+            time.sleep(0.05)
+            drawchess()
+            pygame.display.update()
+            if oldx==newx and oldy==newy:
+                flag=False
+        posdic[tryblock].chess=tempObj
+    animate(firstpos,tryblock,tempObj)
+    if swap:
+        animate(tryblock,firstpos,tempObj2)
+        #posdic[firstpos].chess=tempObj2
+    return
 def main():
     setchess()
     hlightflag=None
@@ -34,12 +76,13 @@ def main():
                     tryblock=cpos
                     print(cpos)
                     chessinblock=posdic[tryblock].chess
-                    move(tryblock,firstpos)
-                    move(55-tryblock,55-firstpos)
+                    swap=False
                     if chessinblock!= None:
                         print('-'*12,chessinblock)
-                        move(firstpos,tryblock)
-                        move(55-firstpos,55-tryblock)
+                        swap=True
+                    move(tryblock,firstpos,swap)
+                    move(55-tryblock,55-firstpos,swap)
+                    
                     firstchess=None
                     
                 elif firstchess==None and blockinfo.chess!=None:
