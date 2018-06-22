f701236c57dbdfd0e94e7b33dc7dd55b989a9c5a
AllanChain <txsmlf@gmail.com>
2018-06-22 17:26:20 +0800

    add poly demo in pygame

---
+++ demo.py
@@ -0,0 +1,18 @@
+from  poly.tile import *
+import pygame
+from pygame.locals import *
+import time
+pygame.init()
+DIS=pygame.display.set_mode((500,500))
+def main():
+     base=poly(n=6,r=40,topleft=(50,50))
+     pg=PolyGroup(base_poly=base,EVEN=4,ODD=4,line=4)
+     for p in pg:
+          pygame.draw.polygon(DIS,(255,255,255),p.points,2)
+     pygame.display.update()
+     while True:
+          time.sleep(0.5)
+          for event in pygame.event.get():
+               if event.type==QUIT:
+                    pygame.quit()
+main()
