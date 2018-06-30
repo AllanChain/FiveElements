4efa1f0dfac1e09345308e87302b9a32cc25c491
AllanChain <txsmlf@gmail.com>
2018-06-30 13:39:21 +0800

    modify poly and its demo

--- demo.py
+++ demo.py
@@ -5,10 +5,11 @@ import time
 pygame.init()
 DIS=pygame.display.set_mode((500,500))
 def main():
-     base=poly(n=8,r=40,topleft=(50,50))
+     base=poly(n=6,r=40,topleft=(50,50))
      pg=PolyGroup(base_poly=base,EVEN=4,ODD=4,line=4)
      for p in pg:
           pygame.draw.polygon(DIS,(255,255,255),p.points,2)
+     pygame.draw.rect(DIS,(255,255,255),pg.rect,2)
      pygame.display.update()
      while True:
           time.sleep(0.2)
