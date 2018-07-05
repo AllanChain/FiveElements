80f9ca9a66227b1046ac08b7772cf1d1d05b822d
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2018-07-05 16:58:02 +0800

    update poly

--- demo.py
+++ demo.py
@@ -6,13 +6,15 @@ pygame.init()
 DIS=pygame.display.set_mode((500,500))
 def main():
      base1=poly(n=6,r=40,topleft=(50,50))
+     base3=poly(n=6,r=40,topleft=(50,270))
      base2=poly(n=8,r=40,topleft=(250,50))
-     pg=ComboGroup((PolyGroup(base_poly=base1,EVEN=4,ODD=4,line=3),\
-                    PolyGroup(base_poly=base2,EVEN=4,ODD=3,line=4)))
+     pg=ComboGroup((PolyGroup(base_poly=base1,EVEN=3,ODD=2,line=3),\
+                    PolyGroup(base_poly=base3,EVEN=3,ODD=2,line=3),\
+                    PolyGroup(base_poly=base2,EVEN=3,ODD=2,line=3)))
      for g in pg.groups:
           for p in g:
                pygame.draw.polygon(DIS,(255,255,255),p.points,2)
-          pygame.draw.rect(DIS,(255,255,255),g.rect,2)
+          pygame.draw.rect(DIS,(255,255,255),g.rect.xywh,2)
      pygame.display.update()
      while True:
           time.sleep(0.2)
