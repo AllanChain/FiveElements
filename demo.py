8d1023358e5155475718890f9f15e04feefeab08
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2018-07-05 17:27:01 +0800

    update poly and try to do on demo first

--- demo.py
+++ demo.py
@@ -1,16 +1,27 @@
-from  poly.tile import *
+import poly
 import pygame
 from pygame.locals import *
 import time
 pygame.init()
-DIS=pygame.display.set_mode((500,500))
+DIS=pygame.display.set_mode((1024,650))
 def main():
-     base1=poly(n=6,r=40,topleft=(50,50))
-     base3=poly(n=6,r=40,topleft=(50,270))
-     base2=poly(n=8,r=40,topleft=(250,50))
-     pg=ComboGroup((PolyGroup(base_poly=base1,EVEN=3,ODD=2,line=3),\
-                    PolyGroup(base_poly=base3,EVEN=3,ODD=2,line=3),\
-                    PolyGroup(base_poly=base2,EVEN=3,ODD=2,line=3)))
+##     base1=poly(n=6,r=40,topleft=(50,50))
+##     base3=poly(n=6,r=40,topleft=(50,270))
+##     base2=poly(n=8,r=40,topleft=(250,50))
+##     pg=ComboGroup((PolyGroup(base_poly=base1,EVEN=3,ODD=2,line=3),\
+##                    PolyGroup(base_poly=base3,EVEN=3,ODD=2,line=3),\
+##                    PolyGroup(base_poly=base2,EVEN=3,ODD=2,line=3)))
+     XMAR=2
+     YMAR=40
+     size=40
+     GAP=size
+     base1=poly.poly(n=8,size=size,topleft=(XMAR,YMAR))
+     base2=poly.poly(n=6,size=size,topleft=(9.2*size+XMAR+GAP,YMAR+0.35*size))
+     base3=poly.poly(n=8,size=size,topleft=(XMAR+14.2*size+XMAR+2*GAP,YMAR))
+     pg=poly.ComboGroup((poly.PolyGroup(base_poly=base1,EVEN=4,ODD=3,line=5),\
+                              poly.PolyGroup(base_poly=base2,EVEN=7,ODD=6,line=3),\
+                              poly.PolyGroup(base_poly=base3,EVEN=4,ODD=3,line=5)))
+
      for g in pg.groups:
           for p in g:
                pygame.draw.polygon(DIS,(255,255,255),p.points,2)
